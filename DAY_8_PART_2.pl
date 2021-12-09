0fb3e92d52b00fcddd7ba48b1d577c0a979f5349
xrt <46675337+TXort@users.noreply.github.com>
2021-12-09 02:40:08 +0100

    Add day 8 solutions

---
+++ DAY_8_PART_2.pl
@@ -0,0 +1,60 @@
+
+# Name:   AOC_2021_DAY8_PART2
+# Author: Ort
+# Date:   2021/12/08
+
+use 5.028;
+
+use strict;
+use warnings;
+no  warnings 'syntax';
+
+use experimental 'signatures';
+use experimental 'lexical_subs';
+
+@ARGV = "C:/Users/Ort/Desktop/input.txt" unless @ARGV;
+
+sub shares ($f, $s) {
+    grep {$s =~ /$_/} split // => $f;
+}
+
+my $part2 = 0;
+while (<>) {
+    chomp;
+    my ($input, $output) = split /\s*\|\s*/;
+    my @input  = map {join "" => sort split //} split ' ' => $input;
+    my @output = map {join "" => sort split //} split ' ' => $output;
+
+    my @bckt;
+    foreach my $i (@input) {
+        push @{$bckt [length $i]} => $i;
+    }
+
+    my @dig;
+
+    $dig [1] = $bckt [2] [0];
+    $dig [4] = $bckt [4] [0];
+    $dig [7] = $bckt [3] [0];
+    $dig [8] = $bckt [7] [0];
+
+    foreach my $try (@{$bckt [6]}) {
+        $dig [shares ($try, $dig [1]) == 1 ? 6
+               : shares ($try, $dig [4]) == 3 ? 0
+               :                                   9] = $try;
+    }
+
+    foreach my $try (@{$bckt [5]}) {
+        $dig [shares ($try, $dig [1]) == 2 ? 3
+               : shares ($try, $dig [9]) == 5 ? 5
+               :                                   2] = $try;
+    }
+
+    my %display;
+    $display {$dig [$_]} = $_ for keys @dig;
+
+    $part2 += join "" => map {$display {$_}} @output;
+}
+
+say $part2;
+
+__END__
